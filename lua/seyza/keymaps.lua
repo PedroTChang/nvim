211ae0a9506b523a5dd25fef4f087ec9caecd8c7
pedrotchang <pedrochang91@gmail.com>
2022-08-18 11:18:34 -0700

    my commit

--- lua/seyza/keymaps.lua
+++ lua/seyza/keymaps.lua
@@ -40,6 +40,10 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- Naviagate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
